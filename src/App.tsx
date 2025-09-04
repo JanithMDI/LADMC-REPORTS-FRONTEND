5d95c8cc86b7305e502dc70bfc506483bbcf0f82
chathuranga <www.1993chathuranga@gmail.com>
2025-09-04 12:51:21 +0530

    check login

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import { Login } from './pages/auth/login';
 import { Dashboard } from './pages/dashboard/Dashboard';
 import Report from './pages/reports/Report';
 
 function App() {
+  const navigate = useNavigate();
+
+  // Redirect to root if jwtToken not found
+  if (!localStorage.getItem("jwtToken")) {
+    navigate("/");
+    return null;
+  }
+
   return (
     <>
       <Routes>
