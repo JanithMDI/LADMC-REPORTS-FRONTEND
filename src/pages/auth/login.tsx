98d01b8aa9041de0c77faa45f1e3cccf24878d2f
Ragavarman <ragav@mdidev.com>
2025-09-11 10:05:29 +0530

    update logo four js

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -5,8 +5,8 @@ import { LoginForm } from './LoginForm'
 export const Login = () => {
   return (
     <>
-        <div className='p-6 w-full flex items-center'>
-            <img src={Logo} alt="LADMC-4JS-LOGO" className='h-12' />
+        <div className='p-6 w-full flex justify-center items-center'>
+            <img src={Logo} alt="LADMC-4JS-LOGO" />
         </div>
         <div className='p-6 md:p-0 w-full md:w-[356px] mt-10 mx-auto flex flex-col gap-8'>
             <h1 className='text-3xl font-semibold capitalize'>Sign in</h1>
