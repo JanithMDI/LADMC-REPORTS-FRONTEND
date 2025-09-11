8055102e99ba5b8df641e00b3a0f0c0a07556811
Ragavarman <ragav@mdidev.com>
2025-09-11 21:49:14 +0530

    Logo resize

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -6,7 +6,7 @@ export const Login = () => {
   return (
     <>
         <div className='p-6 w-full flex  items-center'>
-            <img src={Logo} alt="LADMC-4JS-LOGO" className='h-16'/>
+            <img src={Logo} alt="LADMC-4JS-LOGO" className='h-15'/>
         </div>
         <div className='p-6 md:p-0 w-full md:w-[356px] mt-10 mx-auto flex flex-col gap-8'>
             <h1 className='text-3xl font-semibold capitalize'>Sign in</h1>
